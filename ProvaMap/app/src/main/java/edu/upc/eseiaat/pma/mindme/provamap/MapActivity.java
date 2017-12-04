6ff11c85432c1e0f5d2b22e7038a76a0b7c95af5
bertacanes <bertacanesbadia@gmail.com>
2017-12-04 18:52:15 +0100

    ProvaMap V1. L'aplicació mostra un mapa amb un punter a la ESEIAAT

---
+++ ProvaMap/app/src/main/java/edu/upc/eseiaat/pma/mindme/provamap/MapActivity.java
@@ -0,0 +1,36 @@
+package edu.upc.eseiaat.pma.mindme.provamap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in ESEIAAT, Terrassa, and move the camera.
+        LatLng eseiaat = new LatLng(41.562863, 2.023187);
+        mMap.addMarker(new MarkerOptions().position(eseiaat).title("Marker in ESEIAAT"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(eseiaat));
+    }
+}
