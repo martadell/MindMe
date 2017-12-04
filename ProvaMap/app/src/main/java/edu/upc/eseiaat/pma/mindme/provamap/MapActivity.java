a97c018d4ca0e731df1ef7ee1ae5bb7e23d5dc4d
bertacanes <bertacanesbadia@gmail.com>
2017-12-04 18:58:38 +0100

    ProvaMap V2. L'aplicació mostra dos layouts diferents. Mitjançant un botó es pot canviar de mode de visualització

--- ProvaMap/app/src/main/java/edu/upc/eseiaat/pma/mindme/provamap/MapActivity.java
+++ ProvaMap/app/src/main/java/edu/upc/eseiaat/pma/mindme/provamap/MapActivity.java
@@ -2,6 +2,10 @@ package edu.upc.eseiaat.pma.mindme.provamap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,11 +17,48 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private ViewSwitcher simpleViewSwitcher;
+    Button btnSwitch;
+    Button btn_map;
+    Button btn_pic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        btnSwitch = (Button) findViewById(R.id.btn_switch);
+        btn_map = (Button) findViewById(R.id.btn_map);
+        btn_pic = (Button) findViewById(R.id.btn_pic);
+        simpleViewSwitcher = (ViewSwitcher) findViewById(R.id.viewSwitcher);
+
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                simpleViewSwitcher.showNext();
+                String act_layout = btnSwitch.getText().toString();
+                if (act_layout.equals("TO MAP")){
+                    btnSwitch.setText("TO PIC");
+                }
+                else {
+                    btnSwitch.setText("TO MAP");
+                }
+            }
+        });
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapActivity.this, "Botó map funciona", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapActivity.this, "Botó pic funciona", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
