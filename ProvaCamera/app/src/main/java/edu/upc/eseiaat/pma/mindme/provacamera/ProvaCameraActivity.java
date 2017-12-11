5ff1026fae848e156fa5c1cea7a25a330e00c01f
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-12-11 16:04:31 +0100

    1. Aplicació que crida a la càmera, fa una fotografia i la mostra per pantalla (però s'esborra al girar la pantalla)

--- ProvaCamera/app/src/main/java/edu/upc/eseiaat/pma/mindme/provacamera/ProvaCameraActivity.java
+++ ProvaCamera/app/src/main/java/edu/upc/eseiaat/pma/mindme/provacamera/ProvaCameraActivity.java
@@ -1,13 +1,47 @@
 package edu.upc.eseiaat.pma.mindme.provacamera;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.location.LocationManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProvaCameraActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST =10;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prova_camera);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
+
+    public void btn_camera(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent,CAMERA_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST) {
+                Bitmap cameraImage = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(cameraImage);
+            }
+        }
+    }
+
+
 }
+
