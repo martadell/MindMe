4703447806ddb38aef4e1bb34349d783a59d02d2
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-12-11 16:59:14 +0100

    ProvaCamera 3. Simplificació dades guardades (falta la foto)

--- ProvaCamera/app/src/main/java/edu/upc/eseiaat/pma/mindme/provacamera/ProvaCameraActivity.java
+++ ProvaCamera/app/src/main/java/edu/upc/eseiaat/pma/mindme/provacamera/ProvaCameraActivity.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -17,10 +19,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ProvaCameraActivity extends AppCompatActivity {
 
@@ -31,52 +37,13 @@ public class ProvaCameraActivity extends AppCompatActivity {
     LocationManager locationManager;
     TextView txt_lat, txt_long;
 
-
-    private void writeLastLocation() {
-        try {
-            FileOutputStream fos = openFileOutput("Location.txt", Context.MODE_PRIVATE);
-            String la = txt_lat.getText().toString();
-            String lo = txt_long.getText().toString();
-
-            String line = String.format("%s;%s", la, lo);
-            fos.write(line.getBytes());
-
-            fos.close();
-        } catch (FileNotFoundException e) {
-            Log.e("MindMe", "writeLastLocation: FileNotFoundException");
-            Toast.makeText(this, "Unable to create a save file", Toast.LENGTH_SHORT).show();
-        } catch (IOException e) {
-            Log.e("MindMe", "writeLastLocation: IOException");
-            Toast.makeText(this, "Unable to create a save file", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void readLastLocation() {
-        try {
-            FileInputStream fis = openFileInput("Location.txt");
-            byte[] buffer = new byte[1000];
-            int nread = fis.read(buffer);
-            if (nread > 0) {
-                String content = new String(buffer, 0, nread);
-                String[] location = content.split(";");
-
-                txt_lat.setText(location[0]);
-                txt_long.setText(location[1]);
-            }
-            fis.close();
-        } catch (FileNotFoundException e) {
-            Log.i("MindMe", "FileNotFoundException");
-        } catch (IOException e) {
-            Log.e("MindMe", "readLastLocation: IOException");
-            Toast.makeText(this, "Unable to read the saved file", Toast.LENGTH_SHORT).show();
-        }
-    }
-
     @Override
-    protected void onStop() {
-        super.onStop();
-        writeLastLocation();
-    }
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.i("lifecycle", "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+
+        outState.putString("latitude", txt_lat.getText().toString());
+        outState.putString("longitude", txt_long.getText().toString());}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,8 +56,14 @@ public class ProvaCameraActivity extends AppCompatActivity {
         txt_lat = (TextView) findViewById(R.id.txt_lat);
         txt_long = (TextView) findViewById(R.id.txt_long);
 
-        readLastLocation();
+        if(savedInstanceState == null) {
+            txt_lat.setText("Latitude:");
+            txt_long.setText("Longitude");}
 
+        else {
+            Bundle state = savedInstanceState;
+            txt_lat.setText(state.getString("latitude"));
+            txt_long.setText(state.getString("longitude")); }
     }
 
     public void btn_camera(View view) {
