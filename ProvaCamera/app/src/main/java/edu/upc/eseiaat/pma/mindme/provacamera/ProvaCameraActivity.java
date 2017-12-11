eb9807bdce51bcb47db49eee0f393e75cf3d63f5
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-12-11 16:00:30 +0100

    Afegim ProvaCamera (4)

---
+++ ProvaCamera/app/src/main/java/edu/upc/eseiaat/pma/mindme/provacamera/ProvaCameraActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.mindme.provacamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProvaCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prova_camera);
+    }
+}
