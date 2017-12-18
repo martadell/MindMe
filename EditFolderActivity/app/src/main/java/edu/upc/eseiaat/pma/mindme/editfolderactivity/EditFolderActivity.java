b198a4a890526d9b259c5e187882007c29c54149
Nuria.ricart <nuria.rs96@gmail.com>
2017-12-18 15:20:09 +0100

    7) V15

--- EditFolderActivity/app/src/main/java/edu/upc/eseiaat/pma/mindme/editfolderactivity/EditFolderActivity.java
+++ EditFolderActivity/app/src/main/java/edu/upc/eseiaat/pma/mindme/editfolderactivity/EditFolderActivity.java
@@ -5,6 +5,8 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -15,25 +17,28 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.TreeSet;
 
+import static android.support.v7.appcompat.R.styleable.MenuItem;
+
 public class EditFolderActivity extends AppCompatActivity {
 
     private ArrayList<Categories> llistacat = new ArrayList<Categories>();
     private CategoriesAdapter adapter;
     private String[] noms_cat;
     private int[] imgs;
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_folder);
 
+        //OMPLIM LA LLISTA
+
         noms_cat = getResources().getStringArray(R.array.llistacategories);
         imgs = new int[]{R.drawable.altres, R.drawable.animals, R.drawable.electronica,
         R.drawable.llibres, R.drawable.llocs, R.drawable.menjar, R.drawable.musica,
         R.drawable.regals, R.drawable.roba, R.drawable.transport};
 
-
-        //ARREGLAR ORDRE SPINNER - ACTION BAR per posar la fletxa
 
         for (int i=0; i<noms_cat.length; i++){
             Drawable drawable = getResources().getDrawable(imgs[i], getTheme());
@@ -46,13 +51,34 @@ public class EditFolderActivity extends AppCompatActivity {
             }
         });
 
+        //CREEM L'ADAPTADOR
+
         Spinner llistacategories = (Spinner) findViewById(R.id.llistacategories);
 
         adapter = new CategoriesAdapter(this, llistacat);
 
         llistacategories.setAdapter(adapter);
 
+        //PERSONALITZEM L'ACTION BAR
+
+        /*@Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu items for use in the action bar
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return super.onCreateOptionsMenu(menu);
+        }
+
+
+        onOptionsItemSelected()*/
+
     }
 
+    /*public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
 
 }
