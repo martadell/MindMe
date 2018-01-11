f76255c03858b2092ac6363f1f57e7404422d687
Nuria.ricart <nuria.rs96@gmail.com>
2018-01-11 16:49:32 +0100

    11) Fer menú search a l'action bar

--- MindMe/app/src/main/java/edu/upc/eseiaat/pma/mindme/mindme/FolderListActivity.java
+++ MindMe/app/src/main/java/edu/upc/eseiaat/pma/mindme/mindme/FolderListActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
@@ -21,14 +22,13 @@ public class FolderListActivity extends AppCompatActivity {
     private ArrayList<Carpeta> llista_carpetes;
     private FolderListActivityAdapter adapter;
 
+    //TODO versió 18 ordenar la llista amb un longclick (DragList)
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folder_list);
 
-        /*Intent intent1 = getIntent();
-        int position = intent1.getIntExtra("posicio", 0);
-        llista_carpetes.remove(position);*/
 
         ListView l_c = (ListView) findViewById(R.id.list_folders);
 
@@ -37,7 +37,6 @@ public class FolderListActivity extends AppCompatActivity {
 
         adapter = new FolderListActivityAdapter(this, R.layout.activity_folder_list, llista_carpetes);
         l_c.setAdapter(adapter);
-
 
     }
 
@@ -62,13 +61,14 @@ public class FolderListActivity extends AppCompatActivity {
                     Bitmap bmp = BitmapFactory.decodeByteArray(b, 0, b.length);
                     Drawable d = new BitmapDrawable(getResources(), bmp);
 
-                    if (posicio_carpeta != -1){
-                        llista_carpetes.get(posicio_carpeta).setIcona(d);
-                        llista_carpetes.get(posicio_carpeta).setNom_carpeta(nom);
-                        llista_carpetes.get(posicio_carpeta).setRuta_drawable(ruta_drawable);
-                    }
-                    else {
-                        llista_carpetes.add(new Carpeta(nom, d, ruta_drawable, new ArrayList<Picture>()));
+                    if (!nom.isEmpty()) {
+                        if (posicio_carpeta != -1) {
+                            llista_carpetes.get(posicio_carpeta).setIcona(d);
+                            llista_carpetes.get(posicio_carpeta).setNom_carpeta(nom);
+                            llista_carpetes.get(posicio_carpeta).setRuta_drawable(ruta_drawable);
+                        } else {
+                            llista_carpetes.add(new Carpeta(nom, d, ruta_drawable, new ArrayList<Picture>()));
+                        }
                     }
                     adapter.notifyDataSetChanged();
                 }
@@ -76,18 +76,31 @@ public class FolderListActivity extends AppCompatActivity {
     }
 
     //TODO acitonbar buscar
-    /*@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        getMenuInflater().inflate(R.menu.search_menu, menu);
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
+        MenuItem searchItem = menu.findItem(R.id.menuSearch);
         SearchView searchView =
-                (SearchView) MenuItemCompat.getActionView(searchItem);
+                (SearchView) searchItem.getActionView();
 
-        // Configure the search info and add any event listeners...
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+
+                return false;
+            }
+        });
 
         return super.onCreateOptionsMenu(menu);
-    }*/
+    }
 
 }
